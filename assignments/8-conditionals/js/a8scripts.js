ab63c96d79201dc66754363043a96b1690992560
jssg33 <john@glocation.info>
2026-02-15 20:51:32 -0500

    Update a8scripts.js

--- assignments/8-conditionals/js/a8scripts.js
+++ assignments/8-conditionals/js/a8scripts.js
@@ -1,1 +1,11 @@
-somescripts
+const mobileMenu = document.getElementById("mobilemenu");
+const hidingList = document.getElementById("hiding");
+
+mobileMenu.onclick = function () {
+  // Toggle the list only when mobile mode is active
+  if (window.innerWidth < 1000) {
+    hidingList.style.display =
+      hidingList.style.display === "block" ? "none" : "block";
+  }
+};
+
